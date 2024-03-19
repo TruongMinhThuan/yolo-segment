ddbf4cd684076242908aeebc913a04f2883c146f
flydinogpu <flydinogpu@flydinotech.com>
2024-03-19 16:14:31 +0700

    update

--- main.py
+++ main.py
@@ -2,26 +2,37 @@ from ultralytics import YOLO
 import torch
 import cv2
 
-model = YOLO('yolov8n-seg.pt')  # load a pretrained YOLOv8n segmentation model
+init_model = "yolov8n-seg.pt"
 
-predicted = model.predict("https://nft-snap-dev.s3.ap-northeast-1.amazonaws.com/text-to-img/f3e1d25a-31f1-490d-82d5-7fb2d11c5a79-2024-03-19_01-55-17_image.jpg",
-                          classes=[2]
-                        )
+model = YOLO(init_model)  # load a pretrained YOLOv8n segmentation model
 
-print("Predicted: ",predicted[0].masks)
+
+image_path = "https://nft-snap-dev.s3.ap-northeast-1.amazonaws.com/original_ai_image/zQ05enzaZQntf2BcpOIq1SHv4ANZ59aUQ4TrWR0J.jpg"
+
+predicted = model.predict(
+    source=image_path,
+    classes=[0],
+    conf=0.7,
+    retina_masks=True,
+    device='cpu',
+    iou=0.9
+)
+
+print("Predicted: ", predicted[0].masks)
 if predicted[0].masks is not None:
-  
-  mask = predicted[0].masks[0].data
-  boxes = predicted[0].boxes[0].data
 
-  # clss = boxes[:, 5]
-  # people_indices = torch.where(clss == 0)
-  # people_masks = 0
+    mask = predicted[0].masks[0].data
+    boxes = predicted[0].boxes[0].data
 
-  people_mask = torch.any(mask, dim=0).int() * 255
-  # save to file
-  cv2.imwrite(str('person3_segs.jpg'), people_mask.cpu().numpy())
+    # clss = boxes[:, 5]
+    # people_indices = torch.where(clss == 0)
+    # people_masks = 0
 
+    people_mask = torch.any(mask, dim=0).int() * 255
+    # save to file
+    cv2.imwrite(str('person5_segs.jpg'), people_mask.cpu().numpy())
+else:
+    print("No mask found")
 
 # for result in predicted:
 #     # get array results
