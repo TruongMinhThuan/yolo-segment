daf692dc44cbb19899f8d91e2e3c9e8d1832ebb7
thuantm <thuantm@flydinotech.com>
2024-03-19 13:28:10 +0700

    update

--- main.py
+++ main.py
@@ -4,19 +4,23 @@ import cv2
 
 model = YOLO('yolov8n-seg.pt')  # load a pretrained YOLOv8n segmentation model
 
-predicted = model.predict("https://nft-snap-dev.s3.ap-northeast-1.amazonaws.com/text-to-img/f3e1d25a-31f1-490d-82d5-7fb2d11c5a79-2024-03-19_01-55-17_image.jpg",save=True)
+predicted = model.predict("https://nft-snap-dev.s3.ap-northeast-1.amazonaws.com/text-to-img/f3e1d25a-31f1-490d-82d5-7fb2d11c5a79-2024-03-19_01-55-17_image.jpg",
+                          classes=[2]
+                        )
 
+print("Predicted: ",predicted[0].masks)
+if predicted[0].masks is not None:
+  
+  mask = predicted[0].masks[0].data
+  boxes = predicted[0].boxes[0].data
 
-mask = predicted[0].masks[0].data
-boxes = predicted[0].boxes[0].data
+  # clss = boxes[:, 5]
+  # people_indices = torch.where(clss == 0)
+  # people_masks = 0
 
-# clss = boxes[:, 5]
-# people_indices = torch.where(clss == 0)
-# people_masks = 0
-
-people_mask = torch.any(mask, dim=0).int() * 100
-# save to file
-cv2.imwrite(str('merged_segs.jpg'), people_mask.cpu().numpy())
+  people_mask = torch.any(mask, dim=0).int() * 255
+  # save to file
+  cv2.imwrite(str('person3_segs.jpg'), people_mask.cpu().numpy())
 
 
 # for result in predicted:
